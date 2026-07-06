ef8d802cff6e1618e9c960d6cc8c65ad9aa8e16c
beksmaster <beks.aaa@mail.ru>
2026-07-06 13:24:39 +0600

    test: add integration tests for URL shortening flow

--- src/test/java/kg/megalab/urlshortenerservice/UrlShortenerServiceApplicationTests.java
+++ src/test/java/kg/megalab/urlshortenerservice/UrlShortenerServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package kg.megalab.urlshortenerservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UrlShortenerServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
